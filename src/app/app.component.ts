5539bf00f616ba0ecd29cdf750aa7ceca19b093c
wesley <wesleycontroller@gmail.com>
2023-08-07 07:19:45 -0300

    inicial com seta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wds';
+  form: FormGroup;
+  customOptions: string[] = ['Opção 1', 'Opção 2', 'Opção 3', 'Opção 4'];
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      selectedOption: ['']
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WdsSelectComponent } from './wds-select/wds-select.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WdsSelectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
